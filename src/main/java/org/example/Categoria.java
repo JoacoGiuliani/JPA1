f8393c3250b0b1b1f84d6a436aef7a8297b29454
JoacoGiuliani <joaquinvx1@gmail.com>
2024-09-04 12:11:32 -0300

    Add files via upload

---
+++ src/main/java/org/example/Categoria.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+
+@Entity
+@Table(name = "Categoria")
+public class Categoria implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String denominacion;
+    @ManyToMany(mappedBy = "categorias")
+    private List<Articulo> articulos = new ArrayList<Articulo>();
+}
